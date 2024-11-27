f1934d1484cfe917a8f727111e36c30c2baa045a
amit <amit@openwriteup.com>
2024-11-27 12:24:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,12 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.GCP_CRED_FILE)  # Use the path defined in the environment or variable
+  credentials = file(env.GOOGLE_APPLICATION_CREDENTIALS)  # Use the environment variable injected by Jenkins
   project     = "pelagic-tracker-250700"  # Your GCP project ID
   region      = "asia-south1-c"           # Your desired region
-}
-
-variable "GCP_CRED_FILE" {
-  description = "Path to the GCP credentials JSON file"
-  type        = string
 }
